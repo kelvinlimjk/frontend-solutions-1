f8142bba391d1fa2f1ecedb609c52c343bbf409d
kelvinlimjk <kelvinlim.jk@gmail.com>
2016-10-11 00:57:14 +0800

    Update while-and-do-loops.js

--- program-structure/while-and-do-loops.js
+++ program-structure/while-and-do-loops.js
@@ -3,4 +3,12 @@
  * Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a “#” character. The characters should form a chess board.
  */
 
+var chessBoard = 0; 
+
+var i = 0;
+do {
+   chessBoard += 1;
+   console.log("########\n");
+} while (i < 9);
+
 exports.chessBoard = your_answer
